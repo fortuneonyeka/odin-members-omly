83c6c33d36ee8d16a6ea48e243203d25ccea0fe4
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-08 13:05:13 +0100

    implement create action

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except:[:index]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
